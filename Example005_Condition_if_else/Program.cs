54d91826cf425fbbc104eb31e3a7d1485e8a5c7d
Dmitrych <rmc911@mail.ru>
2023-01-19 13:38:37 +0300

    if else

---
+++ Example005_Condition_if_else/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("enter your name");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "dmitry")
+{
+    Console.WriteLine("Hello Dmitry");
+} 
+else
+{
+    Console.WriteLine("what's your name?");
+}
