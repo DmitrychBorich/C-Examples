57d641af7b1b26a56be56ea668aa1d2abde881f6
Dmitrych <rmc911@mail.ru>
2023-01-19 14:28:59 +0300

    Example006_Bas

---
+++ Example006_Base/Program.cs
@@ -0,0 +1,14 @@
+﻿int a = 3;
+int b = 1;
+int c = 7;
+int d = 2;
+int e = 5;
+
+int max = a;
+    //a > max
+if (max < a) max = a;
+if (max < b) max = b;
+if (max < c) max = c;
+if (max < d) max = d;
+if (max < e) max = e;
+Console.Write(max);
